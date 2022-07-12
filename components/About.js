7324543c3afee8c5950a1f550a665b22c29e29a4
marcos-gonzales <markymarrk@gmail.com>
2022-07-11 20:48:34 -0600

    first sidebar and html entities

--- components/About.js
+++ components/About.js
@@ -7,8 +7,8 @@ const About = () => {
                 <source src="video.mp4" type="video/ogg"/>
             </video>
             <h1 className={styles.about}>Hi, I&apos;m Marcos.</h1>
-            <h1 className={styles.about2}>I&aposm a&nbsp;<span className={styles.coloredText}>Software Engineer.</span></h1>
-            <h2 className={styles.about3}>Below are some of the projects i&aposve worked on.</h2>
+            <h1 className={styles.about2}>I&apos;m a<span className={styles.coloredText}>Software Engineer.</span></h1>
+            <h2 className={styles.about3}>Below are some of the projects i&apos;ve worked on.</h2>
         </>
     )
 }
