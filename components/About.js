11d67b7d117521aec9cea5d39a6fd038b9da9019
marcos-gonzales <markymarrk@gmail.com>
2022-08-03 16:42:38 -0600

    troubleshooting background video

--- components/About.js
+++ components/About.js
@@ -3,9 +3,7 @@ import styles from "../styles/about.module.css"
 const About = () => {
     return (
         <>
-            <video className={styles.video} autoPlay={true} loop={true} muted={true} playsInline={true} disablePictureInPicture={true}>
-                <source src="video.mp4" type="video/ogg"/>
-            </video>
+            <video className={styles.video} autoPlay={true} loop={true} muted={true} playsInline={true} disablePictureInPicture={true} src="video.mp4" />
             <h1 className={styles.about}>Hi, I&apos;m Marcos.</h1>
             <h1 className={styles.about2}>I&apos;m a<span className={styles.coloredText}>Software Engineer.</span></h1>
             <h2 className={styles.about3}>Below are some of the projects i&apos;ve worked on.</h2>
